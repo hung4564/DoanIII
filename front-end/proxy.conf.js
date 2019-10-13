9d89a84d009ff38f498634f729ee5cebd04b9c96
hung.pv <hphamviet83@gmail.com>
2019-10-13 17:10:07 +0700

    init

--- front-end/proxy.conf.js
+++ front-end/proxy.conf.js
@@ -1,7 +1,8 @@
+
 module.exports = {
-  "/alfresco": {
-    target: "http://localhost:8082",
-    secure: false,
-    changeOrigin: true
-  }
+    "/alfresco": {
+        "target": "http://localhost:8082",
+        "secure": false,
+        "changeOrigin": true
+    }
 };
